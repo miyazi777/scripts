b494a19a26798012efc3436003cd68c3f68a4024
TAKESHI MIYAJIMA <takeshimiyajima@takeshinombp.lan>
2020-04-11 18:39:42 +0900

    first commit

---
+++ memo.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+ROOT_DIR="$HOME/memo"
+
+add_file() {
+  dir=$1
+  filename=$2
+  command="$ROOT_DIR/${dir}/${filename}.md"
+  touch $command
+}
+
+list_files() {
+  dir=$1
+  command="$ROOT_DIR/${dir}"
+  ls -l $command
+}
+
+case "$2" in
+  "--list" ) list_files $1 ;;
+  "-l" ) list_files $1 ;;
+  * ) add_file $1 $2 ;;
+esac
