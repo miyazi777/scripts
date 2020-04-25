7310b46359e47a33d400a87ad4481562d2dd7c9a
TAKESHI MIYAJIMA <takeshimiyajima@tdev.lan>
2020-04-26 00:46:46 +0900

    add dev start scripts

---
+++ dev-start.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+cd ~/workspace/zenkigen/HARUTAKA_Server_db
+tmux split-window -h
+tmux resize-pane -R 30
+
+cd ~/workspace/zenkigen/HARUTAKA_Server
+tmux split-window -v
+tmux resize-pane -U 32
+
+tmux split-window -v
+tmux resize-pane -U 10
+
+tmux split-window -v
+
+tmux select-pane -t 1
+tmux split-window -v
+
+tmux select-pane -t 3
+
