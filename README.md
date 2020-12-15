eb1a01045a9d0f193d90ca0bf0e6a50d9838c1e3
miyazi777 <m71203@gmail.com>
2020-12-15 22:34:01 +0900

    add git add shell

--- README.md
+++ README.md
@@ -1,3 +1,4 @@
 個人的なユーティリティシェル
 memo.sh   メモ用のシェルスクリプト
 dev-start.sh  開発用レイアウトシェル(tmux)
+gadd.sh   git addとfzfの組み合わせ
---
+++ gadd.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+function gadd() {
+    local selected
+    selected=$(unbuffer git status -s | fzf -m --ansi --preview="echo {} | awk '{print \$2}' | xargs git diff --color" | awk '{print $2}')
+    if [[ -n "$selected" ]]; then
+        selected=$(tr '\n' ' ' <<< "$selected")
+        git add $selected
+        echo "Completed: git add $selected"
+    fi
+}
+
+gadd
